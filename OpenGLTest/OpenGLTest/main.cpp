d131fda59b88a922c1fb1047f916dbaaa162c2f2
EmWilson <emiliawilson819@gmail.com>
2024-01-10 22:36:16 -0500

    spinning rate

--- OpenGLTest/OpenGLTest/main.cpp
+++ OpenGLTest/OpenGLTest/main.cpp
@@ -197,7 +197,7 @@ int main()
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 projection = glm::mat4(1.0f);
 
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(90.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
