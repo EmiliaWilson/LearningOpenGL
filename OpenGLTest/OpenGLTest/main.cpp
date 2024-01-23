3e7fad07854267223466052b96f9beb5f579f327
EmWilson <emiliawilson819@gmail.com>
2024-01-23 17:43:33 -0500

    using normal vectors to "explode" objects

--- OpenGLTest/OpenGLTest/main.cpp
+++ OpenGLTest/OpenGLTest/main.cpp
@@ -76,7 +76,7 @@ int main()
 
 	// configure global opengl state
 	// -----------------------------
-	//glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 	
 
  
@@ -85,28 +85,8 @@ int main()
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
-	
-	float points[] = {
-		-0.5f, 0.5f, 1.0f, 0.0f, 0.0f, // top-left
-		0.5f, 0.5f, 0.0f, 1.0f, 0.0f,  // top-right
-		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // bottom-right
-		-0.5f, -0.5f, 1.0f, 1.0f, 0.0f // bottom-left
-	};  
 
-
-	// cube VAO
-	unsigned int pointsVAO, pointsVBO;
-	glGenVertexArrays(1, &pointsVAO);
-	glGenBuffers(1, &pointsVBO);
-	glBindVertexArray(pointsVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, pointsVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(points), &points, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
-
-
+	Model backpack("C:\\Users\\Jake\\source\\repos\\EmiliaWilson\\LearningOpenGL\\models\\backpack.obj");
 	
 	// shader configuration
 	// --------------------
@@ -130,14 +110,18 @@ int main()
 		// render
 		// ------
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		glm::mat4 model = glm::mat4(1.0f);
 		shader.use();
-		glBindVertexArray(pointsVAO);
-		glDrawArrays(GL_POINTS, 0, 4);
+		shader.setMat4("projection", projection);
+		shader.setMat4("view", view);
+		shader.setMat4("model", model);
 		
-		
-	
+		shader.setFloat("time", static_cast<float>(glfwGetTime()));
+		backpack.Draw(shader);
 	
 		
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -146,8 +130,6 @@ int main()
 		glfwPollEvents();
     }
 
-	glDeleteVertexArrays(1, &pointsVAO);
-	glDeleteBuffers(1, &pointsVBO);
 
     glfwTerminate();
     return 0;
